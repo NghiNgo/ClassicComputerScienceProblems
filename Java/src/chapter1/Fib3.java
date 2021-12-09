438a0484927c4c78ba68b9331ccec128ab699356
Nghi Ngo <74362909+TuongNghi@users.noreply.github.com>
2021-12-09 21:48:10 +0700

    Initial commit

---
+++ Java/src/chapter1/Fib3.java
@@ -0,0 +1,27 @@
+package chapter1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("boxing")
+public class Fib3 {
+
+	// Map.of() was introduced in Java 9 but returns
+	// an immutable Map
+	// This creates a map with 0->0 and 1->1
+	// Which represent our base cases
+	static Map<Integer, Integer> memo = new HashMap<>(Map.of(0, 0, 1, 1));
+
+	private static int fib3(int n) {
+		if (!memo.containsKey(n)) {
+			// memoization step
+			memo.put(n, fib3(n - 1) + fib3(n - 2));
+		}
+		return memo.get(n);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fib3(5));
+		System.out.println(fib3(40));
+	}
+}
