2a3ebbebb86dd1ff2bfcaba93fdac18b67ed0677
Nghi Ngo <74362909+TuongNghi@users.noreply.github.com>
2021-12-11 22:07:58 +0700

    Day_3

---
+++ Java/src/chapter4/Edge.java
@@ -0,0 +1,21 @@
+package chapter4;
+
+public class Edge {
+	public final int u; // the "from" vertex
+	public final int v; // the "to" vertex
+
+	public Edge(int u, int v) {
+		this.u = u;
+		this.v = v;
+	}
+
+	public Edge reversed() {
+		return new Edge(v, u);
+	}
+
+	@Override
+	public String toString() {
+		return u + " -> " + v;
+	}
+
+}
